65be6c7faa88c52f20b5284fb4885aa5c8ded6f0
tirthpatel <tp680981@gmail.com>
2026-03-23 19:52:45 +0530

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://localhost:5001",
-      "/uploads": "http://localhost:5001",
+      "/api": "https://bakrol-backend.onrender.com",
+      "/uploads": "https://bakrol-backend.onrender.com",
     },
   },
 });
